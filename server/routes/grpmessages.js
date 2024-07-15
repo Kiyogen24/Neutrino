45a4d5a9920ea4aaea3dcaaa365c58477d59205c
Kiyogen <81473648+Reigen24@users.noreply.github.com>
2024-07-15 17:12:52 +0200

    Delete server directory

--- server/routes/grpmessages.js
+++
@@ -1,7 +0,0 @@
-const { sendMessageToGroup, getGroupMessages } = require("../controllers/groupMessageController");
-const router = require("express").Router();
-
-router.post("/addgrpmsg/", sendMessageToGroup);
-router.post("/getgrpmsg/", getGroupMessages);
-
-module.exports = router;
--- server/routes/messages.js
+++
@@ -1,8 +0,0 @@
-const { addMessage, getMessages, getLastMessage } = require("../controllers/messageController");
-const router = require("express").Router();
-
-router.post("/addmsg/", addMessage);
-router.post("/getmsg/", getMessages);
-router.post("/lastmsg/", getLastMessage);
-
-module.exports = router;
